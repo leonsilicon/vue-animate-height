4827473f9fd39e303cc147f9878be121910b692c
Leon Si <contact@leonzalion.com>
2022-04-14 21:23:39 -0400

    fix: build script

--- vue-animate-height/scripts/build.ts
+++ vue-animate-height/scripts/build.ts
@@ -1,8 +1,11 @@
 import { execaCommandSync as exec } from 'execa';
 import { chProjectDir, copyPackageFiles, rmDist } from 'lion-system';
+import * as fs from 'node:fs';
 
 chProjectDir(import.meta.url);
 rmDist();
 exec('tsc');
 exec('tsc-alias');
 await copyPackageFiles({ commonjs: { external: 'vue' } });
+fs.copyFileSync('../readme.md', 'dist/readme.md');
+fs.copyFileSync('../license', 'dist/license');
